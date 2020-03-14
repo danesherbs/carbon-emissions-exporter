7e2e46c24e9dc11af2730371809a0e7d2bfe7a72
Dane Sherburn <dgs13@ic.ac.uk>
2020-03-14 15:29:54 +1100

    Corrected units and carbon emission calculation

--- export.py
+++ export.py
@@ -3,21 +3,21 @@ import pyRAPL
 import time
 
 
-# TODO: fix this constant
-KG_CARBON_PER_KWH = 0.954  # from Emissions & Generation Resource Integrated Database (EPA, 2018)
+# Constants
+KG_CARBON_PER_KWH = 0.4327  # from Emissions & Generation Resource Integrated Database (EPA, 2018)
 PUE = 1.58  # Power Usage Effectiveness (PUE) from (Citation, 2019)
-SAMPLE_DELTA = 0.5  # seconds between samples
+SAMPLE_DUR = 1.0  # seconds to sample
 
 # Prometheus configuration
 start_http_server(8000)
 
-pkg_power_gauge = Gauge('avg_pkg_power', 'Sample average power drawn by CPU (W)')
+pkg_power_gauge = Gauge('avg_pkg_power', 'Sample average power drawn by CPU (kW)')
 pkg_carbon_eq_counter = Counter('pkg_carbon_eq', 'Cumulative carbon emissions produced by CPU (kg)')
 
-dram_power_gauge = Gauge('avg_dram_power', 'Sample average power drawn by DRAM (W)')
+dram_power_gauge = Gauge('avg_dram_power', 'Sample average power drawn by DRAM (kW)')
 dram_carbon_eq_counter = Counter('dram_carbon_eq', 'Cumulative carbon emissions produced by DRAM (kg)')
 
-total_power_gauge = Gauge('avg_total_power', 'Sample average total power drawn (W)')
+total_power_gauge = Gauge('avg_total_power', 'Sample average total power drawn (kW)')
 total_carbon_eq_counter = Counter('total_carbon_eq', 'Cumulative carbon emissions produced in total (kg)')
 
 # Intel RAPL configuration
@@ -28,19 +28,24 @@ meter = pyRAPL.Measurement('devices')
 # Sample power drawn by CPU and DRAM
 while True:
     meter.begin()
-    time.sleep(SAMPLE_DELTA)  # space out samples
+
+    time.sleep(SAMPLE_DUR)  # sample over some time
+
+    if meter.result is not None:
+        duration_hours = meter.result.duration / 60 / 60  # original duration is in seconds
+
+        pkg_energy = sum(meter.result.pkg) / 10 ** 6  # Joules
+        pkg_avg_power = pkg_energy / meter.result.duration / 10 ** 3  # kW
+        pkg_power_gauge.set(pkg_avg_power)
+        pkg_carbon_eq_counter.inc(KG_CARBON_PER_KWH * pkg_avg_power * duration_hours)
+
+        dram_energy = sum(meter.result.dram) / 10 ** 6  # Joules
+        dram_avg_power = dram_energy / meter.result.duration / 10 ** 3  # kW
+        dram_power_gauge.set(dram_avg_power)
+        dram_carbon_eq_counter.inc(KG_CARBON_PER_KWH * dram_avg_power * duration_hours)
+
+        total_avg_power = pkg_avg_power + dram_avg_power / 10 ** 3  # kW
+        total_power_gauge.set(total_avg_power)
+        total_carbon_eq_counter.inc(KG_CARBON_PER_KWH * total_avg_power * duration_hours)
+
     meter.end()
-
-    pkg_energy = sum(meter.result.pkg) / 10 ** 6
-    pkg_avg_power = pkg_energy / meter.result.duration
-    pkg_power_gauge.set(pkg_avg_power)
-    pkg_carbon_eq_counter.inc(KG_CARBON_PER_KWH * pkg_avg_power)
-
-    dram_energy = sum(meter.result.dram) / 10 ** 6
-    dram_avg_power = dram_energy / meter.result.duration
-    dram_power_gauge.set(dram_avg_power)
-    dram_carbon_eq_counter.inc(KG_CARBON_PER_KWH * dram_avg_power)
-
-    total_avg_power = pkg_avg_power + dram_avg_power
-    total_power_gauge.set(total_avg_power)
-    total_carbon_eq_counter.inc(KG_CARBON_PER_KWH * total_avg_power)

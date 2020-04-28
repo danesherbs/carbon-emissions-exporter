2f18e33502293fb69f209cdaa6b8a4049724f414
Dane Sherburn <danesherbs@gmail.com>
2020-04-28 19:33:16 +1000

    Update sources and fix carbon_offset_cost

--- metrics.py
+++ metrics.py
@@ -1,34 +1,35 @@
-from prometheus_client import start_http_server, Gauge, Counter
-import pyRAPL
-import gpumeter
-import time
+"""
+[1] https://www.eea.europa.eu/data-and-maps/daviz/sds/co2-emission-intensity-from-electricity-generation-2/@@view
+[2] Giving What We Can: Cool Earth, Retrieved 28-04-2020, https://www.givingwhatwecan.org/report/cool-earth/#3-overall-evaluation 
+[3] https://calculator.carbonfootprint.com/calculator.aspx?tab=3
+NOTE: [3] is not a legitimate source of where the number came from. We should investigate further
+"""
 
-from meters import *
-
-# Constants
-#     KG_CO2_PER_KWH: An average kg of CO2 per kWh in United Kingdom: 0.281 kg CO2 per kWh [1]
-#     KG_CO2_PER_USD: A pessimistic estimation of the cost to reduce 1 tonne of CO2: $1.34
-#     (could be as low as $0.38 if counted indirectly shielded forests) [2][3]
-#     KG_CO2_PER_FLIGHT: An average kg of CO2 per person during a single trip economy flight from London to New York [4]
-#     Source:
-#     [1] https://www.eea.europa.eu/data-and-maps/daviz/sds/co2-emission-intensity-from-electricity-generation-2/@@view
-#     [2] "ClimateCare." 2006. 12 Jan. 2016 <http://climatecare.org/>
-#     [3] "Carbon Footprint Ltd - Carbon Management Services …" 2003. 12 Jan. 2016 <http://www.carbonfootprint.com/>
-#     [4] https://calculator.carbonfootprint.com/calculator.aspx?tab=3
-#     NOTE: [4] is not a legitimate source of where the number came from. We should investigate further
+"""
+United Kingdom national average for kg of CO2 emissions per kWh [1]
+"""
 KG_CO2_PER_KWH = 0.281
-KG_CO2_PER_USD = 1.34
+
+"""
+Cost (in USD) to offset a kg of CO2 if donated to Cool Earth [2]
+"""
+USD_PER_KG_CO2 = 1.34 / 1000
+
+"""
+Estimate of kg of CO2 per person per one-way flight from London to New York [3]
+"""
 KG_CO2_PER_FLIGHT = 1.67
+
 
 def carbon_offset_cost(kWh):
     """
-    Cost to offset kWh of energy consumption in USD
+    Donation to Cool Earth (in USD) needed to offset carbon emssions.
     """
-    return KG_CO2_PER_KWH * kWh / KG_CO2_PER_USD
+    return KG_CO2_PER_KWH * USD_PER_KG_CO2 * kWh
 
 
 def number_of_flights(kWh):
     """
-    Number of flights from London to New York a given energy consumption (kWh) is equivalent of generating
+    Equivalent number of one-way flights from London to New York.
     """
     return KG_CO2_PER_KWH * kWh / KG_CO2_PER_FLIGHT
